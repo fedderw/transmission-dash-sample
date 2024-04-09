1427f073eb86b3efaa540ff0297d68abca3ae7f7
Will Fedder <fedderw@gmail.com>
2024-04-09 11:45:45 +0000

    Add dash_mantine_components to requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+# Project Overview:
+
+# This Python script aims to facilitate the visualization and analysis of data regarding transmission line permitting, as outlined in the report published by Niskanen in collaboration with the Clean Air Task Force. The dataset encompasses statistics on 37 transmission lines that underwent federal environmental permitting review between 2010 and 2020.
+
+# Goals:
+
+# Data Compilation: The script compiles data scattered across various company and government websites, consolidating it into a cohesive dataset for analysis.
+
+# Visualization Enhancement: The objective is to present the data in a more visually engaging and intuitive manner to broaden its accessibility and usability. This includes:
+
+# Creating an interactive map displaying the 37 transmission lines, akin to the Politico piece referenced.
+# Providing individual project breakouts within the map, showcasing timelines and permitting process statistics.
+# Developing a sortable and interactive version of the dataset, similar to the Brookings model, to facilitate efficient data exploration.
+# Interactive Features: Implementation of interactive features allows users to:
+
+# Sort and filter the dataset based on various parameters.
+# Access additional information and insights about each transmission line project.
+# Potentially incorporate supplementary map layers depicting congressional districts or energy resources.
+
 # Import necessary libraries for Dash and callbacks
 from dash import Dash, html, dcc, callback, Input, Output
 import dash_ag_grid as dag
 import dash_leaflet as dl
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import pandas as pd
 import json
 import geopandas as gpd
@@ -10,7 +30,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import random  # Add this at the beginning of your script
-
+from datetime import datetime
+import numpy as np
+import re
 # Load data for AG Grid and Leaflet Map
 # Number of rows to read (including the header)
 rows_to_read = 38
@@ -26,6 +48,9 @@ eis_lines_gdf = eis_lines_gdf.merge(df_eis_lines, on="Name")
 # print(eis_lines_gdf.head(5))
 # Convert the GeoDataFrame back to GeoJSON
 eis_lines_geojson = json.loads(eis_lines_gdf.to_json())
+
+# Congressional Districts GeoJSON
+# cds = gpd.read_file("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_118th_Congressional_Districts/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
 # print(type(eis_lines_geojson))
 # Initialize Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -50,13 +75,15 @@ def create_tooltip_content(feature):
 # Define layout of the app
 app.layout = dbc.Container(
     children=[
+        html.H1("Transmission Line Permitting Visualization"),
+        html.Hr(),
         dbc.Row(
             [
                 dbc.Col(
                     [
                         dl.Map(
                             center=[37.0902, -95.7129],
-                            zoom=4,
+                            zoom=3,
                             children=[
                                 dl.TileLayer(),
                                 dl.GeoJSON(
@@ -84,18 +111,19 @@ app.layout = dbc.Container(
                             id="leaflet-map",
                         )
                     ],
-                    width=6,
+                    width=4,
                 ),
                 dbc.Col(
                     [
-                        dcc.Graph(
+                         dmc.LoadingOverlay(dcc.Graph(
                             id="gantt-chart"
-                        )  # Placeholder for Gantt chart
+                        ),loaderProps={"variant": "dots", "color": "orange", "size": "xl"},) # Placeholder for Gantt chart
                     ],
-                    width=6,
+                    width=8,
                 ),
             ]
         ),
+        html.Div(style={"height": "30px"}),  # Add space between rows
         dbc.Row(
             [
                 dbc.Col(
@@ -122,6 +150,21 @@ app.layout = dbc.Container(
                 )
             ]
         ),
+        html.Hr(),
+        html.Footer(
+            "For any inquiries, please contact [Your Name] at [Your Email Address]."
+        ),
+        html.Div(
+            [
+                dmc.Button("About", id="drawer-demo-button"),
+                dmc.Drawer(
+                    title="Drawer Example",
+                    id="drawer-simple",
+                    padding="md",
+                    zIndex=10000,
+                ),
+            ]
+        )
     ]
 )
 
@@ -133,31 +176,27 @@ app.layout = dbc.Container(
     ],
 )
 def update_based_on_grid_selection(virtualRowData, selected_rows):
-    if selected_rows is None:
-        selected_rows = []
-    print(f"Selected Rows: {selected_rows}")
-    print(f"Virtual Row Data preview: {virtualRowData[:1]}")
+    gdff = eis_lines_gdf if virtualRowData is None else eis_lines_gdf.merge(pd.DataFrame(virtualRowData), on="Name", how="inner")
+    selected_names = [s["Name"] for s in selected_rows] if selected_rows else []
     
-    selected_names = [virtualRowData[i]['Name'] for i in selected_rows] if selected_rows else []
-    print(f"Selected Names: {selected_names}")
-    if not selected_names:
-        # If no rows are selected, show all data
-        geojson_layer = dl.GeoJSON(data=eis_lines_geojson, id="geojson")
+    if selected_names:
+        filtered_gdf = gdff[gdff["Name"].isin(selected_names)]
+        filtered_geojson = json.loads(filtered_gdf.to_json())
     else:
-        # Filter the GeoJSON data based on selected rows
-        filtered_features = [
-            feature
-            for feature in eis_lines_geojson["features"]
-            if feature["properties"]["Name"] in selected_names
-        ]
-        geojson_layer = dl.GeoJSON(data={"type": "FeatureCollection", "features": filtered_features}, id="geojson")
-
-    # Return the updated children for the map
+        filtered_gdf = gdff
+        filtered_geojson = json.loads(filtered_gdf.to_json())
+        
+    # filtered_geojson = json.loads(filtered_gdf.to_json())
+    # Update the chldren of "map-geojson" with the filtered GeoJSON
     return [
-        dl.TileLayer(),  # Default OpenStreetMap layer
-        geojson_layer,
+        dl.TileLayer(),
+        dl.GeoJSON(data=filtered_geojson, id="map-geojson", children=[
+            dl.Tooltip(
+                children=create_tooltip_content(feature),
+                sticky=True
+            ) for feature in filtered_geojson["features"]
+        ])
     ]
-    
     
 @app.callback(
     Output("gantt-chart", "figure"),
@@ -183,31 +222,92 @@ def update_gantt_chart(rows, selected_rows):
         filtered_df["Date last ROD published"], errors="coerce"
     )
 
-    # Create figure for the timeline
-    fig = px.timeline(
-        filtered_df,
-        x_start="Date of NOI Publication",
-        x_end="Date last ROD published",
-        y="Name",
-        labels={"Name": "Project Name"},
-        title="Project Timeline",
-    )
+
+    # Define color mapping
+    color_mapping = {
+        'Complete': '#66c2a5',  # Light green
+        'Canceled': '#fc8d62',  # Light orange
+        'Underway': '#8da0cb',  # Light blue
+        'Unknown': '#e78ac3',  # Light pink
+    }
+
+    # Handle 'NaT' values and trailing spaces in 'Status of NEPA review' column
+    filtered_df['Status of NEPA review'] = filtered_df['Status of NEPA review'].fillna('Unknown').str.strip()
+
+    # Set 'Date last ROD published' to today's date for 'Underway' processes
+    filtered_df.loc[filtered_df['Status of NEPA review'] == 'Underway', 'Date last ROD published'] = datetime.now()
+    # Replace 'in progress' with NaN
+    filtered_df['Year Federal EIS Issued'] = filtered_df['Year Federal EIS Issued'].replace('in progress', np.nan)
+
+
+    # Convert 'Energized?' column to string and extract year
+    filtered_df['Year Energized'] = filtered_df['Energized?'].astype(str).apply(lambda x: re.findall(r'\b\d{4}\b', x))
+    filtered_df['Year Energized'] = filtered_df['Year Energized'].apply(lambda x: x[0] if x else np.nan)
+
+    # Create separate dataframes for each phase
+    df_proposed = filtered_df[['Name', 'Year project proposed']].copy()
+    df_proposed['Phase'] = 'Proposed'
+    df_proposed['Start'] = pd.to_datetime(df_proposed['Year project proposed'], format='%Y')
+    df_proposed['Finish'] = pd.to_datetime(filtered_df['Date of NOI Publication'])
+
+    df_noi = filtered_df[['Name', 'Date of NOI Publication']].copy()
+    df_noi['Phase'] = 'NOI Published'
+    df_noi['Start'] = pd.to_datetime(df_noi['Date of NOI Publication'])
+    df_noi['Finish'] = pd.to_datetime(filtered_df['Year Federal EIS Issued'], format='%Y')
+
+    df_eis = filtered_df[['Name', 'Year Federal EIS Issued']].copy()
+    df_eis['Phase'] = 'EIS Issued'
+    df_eis['Start'] = pd.to_datetime(df_eis['Year Federal EIS Issued'], format='%Y')
+    df_eis['Finish'] = pd.to_datetime(filtered_df['Date last ROD published'])
+
+    df_energized = filtered_df[['Name', 'Year Energized']].copy()
+    df_energized['Phase'] = 'Energized'
+    df_energized['Start'] = pd.to_datetime(df_energized['Year Energized'], format='%Y')
+    df_energized['Finish'] = pd.to_datetime(df_energized['Year Energized'], format='%Y')
+
+    # Handle 'in progress' status
+    df_in_progress = filtered_df[filtered_df['Energized?'] == 'In progress'][['Name']].copy()
+    df_in_progress['Phase'] = 'In progress'
+    df_in_progress['Start'] = pd.to_datetime(filtered_df['Year Federal EIS Issued'], format='%Y')
+    df_in_progress['Finish'] = pd.to_datetime(datetime.now().year, format='%Y')
+
+    # Concatenate the dataframes
+    df_timeline = pd.concat([df_proposed, df_noi, df_eis, df_energized, df_in_progress])
+
+    # Create a timeline
+    fig = px.timeline(df_timeline, x_start='Start', x_end='Finish', y='Name', color='Phase')
 
     # Update layout for better readability
     fig.update_layout(
         {
-            "xaxis_title": "Date",
-            "yaxis_title": "Project",
+            "height": 600,  # Increase the height of the chart
+            "bargap": 0.2,  # Add spacing between the bars
+            "yaxis_title": None,
+            "xaxis_title": None, 
             "yaxis": {
                 "autorange": "reversed"
             },  # Reverse axis so it goes top-down
-            "showlegend": False,
+            "showlegend": True,  # Show legend
+            "legend": {
+                "orientation": "h",  # Horizontal orientation
+                "yanchor": "bottom",
+                "y": 1.02,  # Position it above the chart
+                "xanchor": "right",
+                "x": 1
+            },
         }
     )
 
     # Return the figure
     return fig
 
+@callback(
+    Output("drawer-simple", "opened"),
+    Input("drawer-demo-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def drawer_demo(n_clicks):
+    return True
 # Run the Dash app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True,dev_tools_hot_reload_watch_interval=5)
